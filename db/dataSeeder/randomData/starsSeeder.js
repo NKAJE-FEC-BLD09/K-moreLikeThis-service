d5003df0fd6226f35bf8ee2d52dd7042d9503c6a
Kyle Tilley <katilley10@gmail.com>
2020-05-03 14:29:42 -0600

    test 2

--- db/dataSeeder/randomData/starsSeeder.js
+++ db/dataSeeder/randomData/starsSeeder.js
@@ -4,7 +4,7 @@ const starsSeeder = () => {
   const count = Math.floor((Math.random() * 3) + 1);
   let stars = [];
   for (let i = 0; i < count; i++) {
-    stars.push(faker.fake('{{name.fisrtName}} {{name.lastName}}'));
+    stars.push(faker.fake('{{name.firstName}} {{name.lastName}}'));
   }
   return stars;
 }
