cd02bf1a5269b5c48d7a54e4559388f1721b2511
Kyle Tilley <katilley10@gmail.com>
2020-05-08 12:16:28 -0600

    first successful test

--- db/db.test.js
+++ db/db.test.js
@@ -1,2 +1,13 @@
 const chai = require('chai');
 const expect = chai.expect;
+
+const getMovie = require('./queries/getMovie.js');
+
+describe('db test suite', () => {
+  describe('getMovie query function', () => {
+    it('should return false when the input is not a number', () => {
+      const result = getMovie('a');
+       expect(result).to.equal(false);
+    })
+  })
+})
--- db/queries/getMovie.js
+++ db/queries/getMovie.js
@@ -1,6 +1,9 @@
 const Movie = require('../MovieSchema.js');
 
 const getMovie = (id) => {
+  if (typeof id !== 'number') {
+    return false;
+  }
   return Movie.findOne({'movieID': id});
 }
 
