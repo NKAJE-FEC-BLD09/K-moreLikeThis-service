71ede2a91e3578eff02769e48f740cbb09d5f1b5
Kyle Tilley <katilley10@gmail.com>
2020-05-12 21:29:14 -0600

    description.jsx is linted

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './description.css';
 
-const Description = (props) => {
-  return (
+const Description = () => (
   <div className="description">
-    <img className="description-image" src='https://upload.wikimedia.org/wikipedia/en/3/31/The_SpongeBob_SquarePants_Movie_poster.jpg' alt='SpongeBob' />
+    <img className="description-image" src="https://upload.wikimedia.org/wikipedia/en/3/31/The_SpongeBob_SquarePants_Movie_poster.jpg" alt="spongeBob" />
 
   </div>
-  )
-}
+);
+
 
 export default Description;
