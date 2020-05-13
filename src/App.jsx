dfcfa3c9e7f969fc96ef0d341dd0a0a7025c7a61
Kyle Tilley <katilley10@gmail.com>
2020-05-12 20:02:34 -0600

    lint is gross. So is linter

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,38 @@
 import React from 'react';
 import './App.css';
 
-import Title from './components/title/Title.jsx';
-import Description from './components/description/Description.jsx';
-import Carousel from './components/carousel/Carousel.jsx';
-
+import Title from './components/title/Title';
+import Description from './components/description/Description';
+import Carousel from './components/carousel/Carousel';
 
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      currentMovie: [],
       similarMovies: [],
-    }
+    };
   }
 
   componentDidMount() {
-    fetch(`http://localhost:8153/movies/1`)
-      .then(res => res.json())
+    fetch('http://localhost:8153/movies/1')
+      .then((res) => res.json())
       .then((data) => {
-        data.similarMovies.forEach(movie => {
+        data.similarMovies.forEach((movie) => {
           fetch(`http://localhost:8153/movies/${movie}`)
-            .then(res => res.json())
-            .then(data => {
-              let similarArr = this.state.similarMovies;
-              if (similarArr.length === 6) { //remove me when I figure out how to make the page turn of the carousel
+            .then((res) => res.json())
+            .then((newData) => {
+              const similarArr = this.state.similarMovies;
+              if (similarArr.length === 6) { //remove me when carousel
                 return;
               }
-              this.setState({ similarMovies: [...similarArr, data] })
-            })
+              this.setState({ similarMovies: [...similarArr, newData] });
+            });
         });
-      })
+      });
   }
 
-  //functions
 
   render() {
     return (
@@ -52,7 +49,7 @@ class App extends React.Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import Movie from '../movie/Movie.jsx';
+import Movie from '../movie/Movie';
 import './carousel.css';
 
-const Carousel = ( {movies} ) => {
-  return (
-    <div className="carousel">
-      {movies.map(movie =>
-        <Movie key={movie.movieID} movie={movie} />
-      )}
-    </div>
-  )
-}
+const Carousel = ({ movies }) => (
+
+  <div className="carousel">
+    {movies.map(movie =>
+      <Movie key={movie.movieID} movie={movie} />
+    )}
+  </div>
+)
+
 
 export default Carousel;
