3ea637c68c50dfd7c57b9f907f7aa2882900cd21
Kyle Tilley <katilley10@gmail.com>
2020-05-14 12:31:14 -0600

    sizing and colors are done for review box. next comes location

--- src/components/description/ReviewScore.jsx
+++ src/components/description/ReviewScore.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import StarRatingComponent from 'react-star-rating-component';
+import './description.css';
 
 const ReviewScore = ({ reviewScore }) => {
   const onStarClick = (e) => {
@@ -9,11 +10,19 @@ const ReviewScore = ({ reviewScore }) => {
   return (
     <div className="ringo-star">
       <StarRatingComponent
+        className="stars"
         name="rating"
         starCount={10}
         value={reviewScore}
         onStarClick={onStarClick}
+        starColor="#a58600"
       />
+      <span className="score">
+        {reviewScore}
+        <span className="divisor">
+          /10
+        </span>
+      </span>
     </div>
   );
 };
