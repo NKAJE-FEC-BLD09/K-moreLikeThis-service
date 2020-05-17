21744ade90ea1af4e63722bc452ea893a2d30c99
Kyle Tilley <katilley10@gmail.com>
2020-05-17 17:16:22 -0600

    added sound file but couldn't get it to play

--- src/components/description/DescriptionImage.jsx
+++ src/components/description/DescriptionImage.jsx
@@ -6,27 +6,34 @@ import './description.css';
 const watchList = React.createElement('button', { type: 'button' }, 'Add to Watchlist');
 const next = React.createElement('button', { type: 'button' }, 'next »');
 
-const Description = ({ movie, onNextButtClick }) => (
+const Description = ({ movie, onNextButtClick }) => {
+  const audio = new Audio('i hate computers.mp3');
+  const addClick = (e) => {
+    e.preventDefault();
+    audio.play();
+  };
 
-  <div className="descriptionSplit">
-    <div className="image-watchlist">
-      <img className="description-image" src={movie.image} alt="spongeBob" />
+  return (
+    <div className="descriptionSplit">
+      <div className="image-watchlist">
+        <img className="description-image" src={movie.image} alt="spongeBob" />
+      </div>
+      <div className="description-words">
+        <DescriptionWords movie={movie} />
+      </div>
+      <div className="add-to-watch">
+        {watchList}
+      </div>
+      <div
+        className="next"
+        role="presentation"
+        onClick={(e) => onNextButtClick(e)}
+      >
+        {next}
+      </div>
     </div>
-    <div className="description-words">
-      <DescriptionWords movie={movie} />
-    </div>
-    <div className="add-to-watch">
-      {watchList}
-    </div>
-    <div
-      className="next"
-      role="presentation"
-      onClick={(e) => onNextButtClick(e)}
-    >
-      {next}
-    </div>
-  </div>
-);
+  );
+};
 
 Description.propTypes = {
   movie: PropTypes.shape({ image: PropTypes.string }).isRequired,
