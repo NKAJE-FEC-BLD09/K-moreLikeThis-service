976b5af7b026b18c863ae8da0da8aa3a04020747
Kyle Tilley <katilley10@gmail.com>
2020-05-17 17:51:42 -0600

    you win this round

--- src/components/description/DescriptionImage.jsx
+++ src/components/description/DescriptionImage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Sound from 'react-sound';
 import PropTypes from 'prop-types';
 import DescriptionWords from './DescriptionWords';
 import './description.css';
@@ -7,40 +6,27 @@ import './description.css';
 const watchList = React.createElement('button', { type: 'button' }, 'Add to Watchlist');
 const next = React.createElement('button', { type: 'button' }, 'next »');
 
-const Description = ({ movie, onNextButtClick }) => {
-  const audio = new Audio('i hate computers.mp3');
-  const addClick = (e) => {
-    e.preventDefault();
-    return (
-      <Sound
-        url="i_hate_computers.mp3"
-        volume={90}
-        playStatus={Sound.status.PLAYING}
-      />
-    );
-  };
-
-  return (
-    <div className="descriptionSplit">
-      <div className="image-watchlist">
-        <img className="description-image" src={movie.image} alt="spongeBob" />
-      </div>
-      <div className="description-words">
-        <DescriptionWords movie={movie} />
-      </div>
-      <div className="add-to-watch">
-        <button type="button" onClick={(e) => addClick(e)}>Add to Watchlist</button>
-      </div>
-      <div
-        className="next"
-        role="presentation"
-        onClick={(e) => onNextButtClick(e)}
-      >
-        {next}
-      </div>
+const Description = ({ movie, onNextButtClick }) => (
+  <div className="descriptionSplit">
+    <div className="image-watchlist">
+      <img className="description-image" src={movie.image} alt="spongeBob" />
     </div>
-  );
-};
+    <div className="description-words">
+      <DescriptionWords movie={movie} />
+    </div>
+    <div className="add-to-watch">
+      {watchList}
+    </div>
+    <div
+      className="next"
+      role="presentation"
+      onClick={(e) => onNextButtClick(e)}
+    >
+      {next}
+    </div>
+  </div>
+);
+
 
 Description.propTypes = {
   movie: PropTypes.shape({ image: PropTypes.string }).isRequired,
